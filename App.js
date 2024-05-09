91cba161be67ac9b37dc7884713a978e35084cc2
18hailey <hailey9459@gmail.com>
2024-05-10 04:18:28 +0900

    setting

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Expo DevTools Test');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to the React Native World!</Text>
       <StatusBar style="auto" />
     </View>
   );
