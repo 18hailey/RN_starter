89400eded15f4f726c529e5d8f0d6eb598402d44
18hailey <hailey9459@gmail.com>
2024-05-10 04:37:00 +0900

    컴포넌트로 정리

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default function App() {
-  console.log('Expo DevTools Test');
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to the React Native World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
